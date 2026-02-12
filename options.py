17ece7abf86f4538b17b1ef2930d6f0f6da44c6f
Owen Yi <owenyi333@gmail.com>
2026-02-12 16:35:54 +1100

    multiple symbols

--- options.py
+++ options.py
@@ -12,19 +12,28 @@ Based off of Roman Paolucci's https://github.com/romanmichaelpaolucci/Quant-Guil
 """
 
 class GetData(EWrapper, EClient):
-    def __init__(self):
+    def __init__(self, symbols = ["3690", "5", "700"]):
         EClient.__init__(self, self)
+
+        self.symbols = symbols
 
         self.connected_event = threading.Event()
         self.underlying_contract_details_event = threading.Event()
         self.spot_price_event = threading.Event()
         self.chain_resolved = threading.Event()
+        
+        self.underlying_conId = {} 
+        self.underlying_conId_id_map = {}
+
+        self.option_chain_id_map = {}
+        self.strikes = {}
+        self.expirations = {}
+        self.spot_id_map = {}
+        self.spot_price = {}
 
         self.next_order_id = None
 
-        self.id_map = {}
-        self.expirations = []
-        self.strikes = []
+        self.option_id_map = {}
 
         self.file = open("data.log", "a")
 
@@ -37,17 +46,26 @@ class GetData(EWrapper, EClient):
         self.connected_event.set()   # API is ready
 
     def contractDetails(self, reqId, contractDetails):
-        self.underlying_conId = contractDetails.contract.conId
-        self.underlying_contract_details_event.set()
+        assert reqId in self.underlying_conId_id_map
+        symbol = self.underlying_conId_id_map[reqId]
+
+        self.underlying_conId[symbol] = contractDetails.contract.conId
+
+        if self.underlying_conId.keys() == set(self.symbols):
+            self.underlying_contract_details_event.set()
 
     def tickPrice(self, reqId, tickType, price, attrib):
-        if reqId == 999 and tickType in [68] and price > 0:
-            self.spot_price = price
-            self.spot_price_event.set()
-        elif reqId in self.id_map and tickType in [66, 67, 68] and price > 0:
-            exp, strike, right = self.id_map[reqId]
+        if reqId in self.spot_id_map and tickType in [68] and price > 0:
+            symbol = self.spot_id_map[reqId]
+            self.spot_price[symbol] = price
+
+            if self.spot_price.keys() == set(self.symbols):
+                self.spot_price_event.set()
+
+        elif reqId in self.option_id_map and tickType in [66, 67, 68] and price > 0:
+            symbol, exp, strike, right = self.option_id_map[reqId]
             now = datetime.now().astimezone().strftime("%Y-%m-%d %H:%M:%S %Z %z")
-            print(f"time: {now}, reqId: {reqId}, tickType: {TickTypeEnum.toStr(tickType)}, price: {price}, attrib: {attrib}, expiry: {exp}, strike: {strike}, right: {right}")
+            print(f"time: {now}, reqId: {reqId}, tickType: {TickTypeEnum.toStr(tickType)}, price: {price}, attrib: {attrib}, symbol: {symbol}, expiry: {exp}, strike: {strike}, right: {right}")
             
     def tickSize(self, reqId, tickType, size):
         tick_types = [
@@ -55,21 +73,26 @@ class GetData(EWrapper, EClient):
             70, # delayed ask size
             71, # delayed last size
                 ]
-        if tickType in tick_types:
+        if reqId in self.option_id_map and tickType in tick_types:
+            symbol, exp, strike, right = self.option_id_map[reqId]
             now = datetime.now().astimezone().strftime("%Y-%m-%d %H:%M:%S %Z %z")
-            print(f"time: {now}, reqId: {reqId}, tickType: {TickTypeEnum.toStr(tickType)}, size: {size}")
-            print(f"time: {now}, reqId: {reqId}, tickType: {TickTypeEnum.toStr(tickType)}, size: {size}, expiry: {exp}, strike: {strike}, right: {right}")
+            print(f"time: {now}, reqId: {reqId}, tickType: {TickTypeEnum.toStr(tickType)}, size: {size}, symbol: {symbol}, expiry: {exp}, strike: {strike}, right: {right}")
 
 
     # Callback receiving the list of strikes and expirations for the asset
     def securityDefinitionOptionParameter(self, reqId, exchange, underlyingConId, tradingClass, multiplier, expirations, strikes):
-        self.expirations = sorted(list(expirations))
-        self.strikes = sorted(list(strikes))
-        self.chain_resolved.set()
+        assert reqId in self.option_chain_id_map
+        symbol = self.option_chain_id_map[reqId] 
+
+        self.expirations[symbol] = sorted(list(expirations))
+        self.strikes[symbol] = sorted(list(strikes))
+
+        if self.strikes.keys() == set(self.symbols) and self.expirations.keys() == set(self.symbols):
+            self.chain_resolved.set()
 
     def error(self, reqId, errorCode, errorString, advancedOrderReject=""):
-        if reqId in self.id_map:
-            exp, strike, right = self.id_map[reqId] 
+        if reqId in self.option_id_map:
+            symbol, exp, strike, right = self.option_id_map[reqId] 
             print(f"exp: {exp}, strike: {strike}, right: {right}", end = ", ")
         print(
             f"reqId: {reqId}, "
@@ -99,8 +122,8 @@ class GetData(EWrapper, EClient):
 def start_app():
     app = GetData()
 
-    # app.connect("127.0.0.1", 7496, clientId=1)
-    app.connect("127.0.0.1", 4002, clientId=1)
+    app.connect("127.0.0.1", 7496, clientId=1)
+    # app.connect("127.0.0.1", 4002, clientId=1)
 
     api_thread = threading.Thread(target=app.run, daemon=True)
     api_thread.start()
@@ -112,62 +135,90 @@ def start_app():
     app.reqMarketDataType(3)  # Delayed data
     return app
 
-def run_app(app): 
-    symbol = "3690"
+# TODO: it's a bit retarded to have all stocks resolve before proceeding to the next stage. If one stock fails, it might bring the rest down. Better to have independent processes for each stock running in parallel. 
+
+# TODO: also need to store stock prices
+
+def run_app(app):  
     exchange = "SEHK"
     currency = "HKD"
+    
+    req_id = 1
+    
+    # GRAB CONTRACT DETAILS
+    for symbol in app.symbols:
+        underlying = Contract()
+        underlying.symbol = symbol
+        underlying.secType = 'STK'
+        underlying.exchange = exchange 
+        underlying.currency = currency
 
-    underlying = Contract()
-    underlying.symbol = symbol
-    underlying.secType = 'STK'
-    underlying.exchange = exchange 
-    underlying.currency = currency
+        app.reqContractDetails(req_id, underlying)
+        app.underlying_conId_id_map[req_id] = symbol
 
-    app.reqContractDetails(1, underlying)
+        req_id += 1
+
     if not app.underlying_contract_details_event.wait(timeout=5):
         raise RuntimeError("Contract Details didn't resolve?")
     
-    print("Underlying Contract ID", app.underlying_conId)
-    app.reqSecDefOptParams(2, symbol, "", "STK", app.underlying_conId)
+    print("Contract Details")
+
+    # GRAB OPTION CHAIN 
+    for symbol in app.symbols:
+        app.reqSecDefOptParams(req_id, symbol, "", "STK", app.underlying_conId[symbol])
+        app.option_chain_id_map[req_id] = symbol
+
+        req_id += 1
+
     if not app.chain_resolved.wait(timeout=5):
         raise RuntimeError("Option Chain didn't resolve?")
-     
-    app.reqMktData(999, underlying, "", False, False, [])
+    
+    print("Option Chain")
+   
+    # GRAB SPOT PRICES
+    for symbol in app.symbols:
+        underlying = Contract()
+        underlying.symbol = symbol
+        underlying.secType = 'STK'
+        underlying.exchange = exchange 
+        underlying.currency = currency
+
+        app.reqMktData(req_id, underlying, "", False, False, [])
+        app.spot_id_map[req_id] = symbol
+        req_id += 1
+    
     if not app.spot_price_event.wait(timeout = 60):
         raise RuntimeError("Could not receive Spot Price")
-    spot = app.spot_price
     
-    print("Expirations", app.expirations)
-    print("Strikes", app.strikes)
-    
+    print("Spot") 
+
+    # GRAB OPTION PRICES
     today = time.strftime('%Y%m%d')
-    # target_exps = [e for e in app.expirations if e >= today][:6]
-    target_exps = ["20260330", "20260429"]
 
-    # grab middle 20 strikes
-    n = len(app.strikes) # TODO: strikes range from 50 to 320, half way is  185 and no one is trading at 185
+    for symbol in app.symbols: 
+        spot = app.spot_price[symbol]
 
-    k = 5 # how many strikes away
-    # target_strikes = app.strikes[n//2 - k: n//2 + k] 
-    target_strikes = [80, 90, 100]
+        target_exps = [e for e in app.expirations[symbol] if e >= today][:6]
 
-    req_id = 1000
-    for exp in target_exps:
-        for strike in target_strikes:
-            opt = Contract()
-            opt.symbol = symbol
-            opt.secType = 'OPT'
-            opt.exchange = exchange
-            opt.currency = currency
-            opt.lastTradeDateOrContractMonth = exp
-            opt.strike = strike
+        k = 0.1 
+        target_strikes = [s for s in app.strikes[symbol] if spot * (1-k) <= s <= spot * (1+k)]
 
-            right = 'C' if strike >= spot else 'P' # Use Calls for OTM/ITM split based on spot
-            opt.right = right
-            app.id_map[req_id] = (exp, strike, right)
-            app.reqMktData(req_id, opt, "", False, False, [])
-            req_id += 1
-            time.sleep(0.01) # Avoid rate-limiting the API
+        for exp in target_exps:
+            for strike in target_strikes:
+                opt = Contract()
+                opt.symbol = symbol
+                opt.secType = 'OPT'
+                opt.exchange = exchange
+                opt.currency = currency
+                opt.lastTradeDateOrContractMonth = exp
+                opt.strike = strike
+
+                right = 'C' if strike >= spot else 'P' # Use Calls for OTM/ITM split based on spot
+                opt.right = right
+                app.option_id_map[req_id] = (symbol, exp, strike, right)
+                app.reqMktData(req_id, opt, "", False, False, [])
+                req_id += 1
+                time.sleep(0.01) # Avoid rate-limiting the API
 
     return app
 
